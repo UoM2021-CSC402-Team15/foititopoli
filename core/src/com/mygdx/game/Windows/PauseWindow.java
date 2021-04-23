073281c0304d6da85b82eade38ba861e6adf3196
Matzi24GR <ggeomat@gmail.com>
2021-04-23 16:30:33 +0300

    Added Pause Screen

---
+++ core/src/com/mygdx/game/Windows/PauseWindow.java
@@ -0,0 +1,50 @@
+package com.mygdx.game.Windows;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Window;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.mygdx.game.Foititopoli;
+import com.mygdx.game.Screens.MainMenuScreen;
+
+
+public class PauseWindow extends Window{
+
+    public PauseWindow(String title, Skin skin, final Foititopoli game) {
+        super(title, skin);
+
+        this.setModal(true);
+
+        TextButton resumeButton = new TextButton("Resume", Foititopoli.gameSkin);
+        resumeButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                PauseWindow.this.remove();
+            }
+        });
+        final TextButton saveButton = new TextButton("Save", Foititopoli.gameSkin);
+        saveButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                saveButton.setText("Game Saved");
+            }
+        });
+        TextButton exitButton = new TextButton("Exit to Main Menu", Foititopoli.gameSkin);
+        exitButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                game.setScreen(new MainMenuScreen(game));
+            }
+        });
+
+        add(resumeButton).row();
+        add(saveButton).row();
+        add(exitButton);
+    }
+
+    @Override
+    public void setPosition(float x, float y) {
+        super.setPosition(x-getWidth()/2, y-getHeight()/2);
+    }
+}
